bba1e2201421606e79b8ec8ed3faa36e4aead13d
xsilv4 <pedrobispoalves77@gmail.com>
2024-10-17 02:50:05 -0300

    push: Tabuada

---
+++ index.py
@@ -0,0 +1,21 @@
+
+name = input("Digite seu nome: ")
+print("Boa noite, " + name + "!")
+print("Vamos praticar um pouco de tabuada!")
+
+
+# Começo da Tabuada 
+
+num = int(input("Digite um número: "))
+print('-' *14)
+print("{} x {:2} = {}".format(num, 1, num*1))
+print("{} x {:2} = {}" .format(num, 2, num*2))
+print("{} x {:2} = {}" .format(num, 3, num*3))
+print("{} x {:2} = {}" .format(num, 4, num*4))
+print("{} x {:2} = {}" .format(num, 5, num*5))
+print("{} x {:2} = {}" .format(num, 6, num*6))
+print("{} x {:2} = {}" .format(num, 7, num*7))
+print("{} x {:2} = {}" .format(num, 8, num*8))
+print("{} x {:2} = {}" .format(num, 9, num*9))
+print("{} x {:2} = {}" .format(num, 10, num*10))
+print('-' *14)
